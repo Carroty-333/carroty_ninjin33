0c10f67ed44e111c9250c391be0e4651c31f32a0
Carroty <carrotninjin0423@gmail.com>
2026-09-04 23:10:48 +0900

    FAギャラリーにねこきのさん作品を追加

--- js/script.js
+++ js/script.js
@@ -241,6 +241,7 @@
     { name: "ねこきのさん", src: "assets/gallery/fa-115-nekokino.jpg", twitter: "" },
     { name: "くーさん", src: "assets/gallery/fa-116-ku.jpg", twitter: "" },
     { name: "EXさん", src: "assets/gallery/fa-117-ex.jpg", twitter: "https://x.com/EX72913880/status/2095134349352218646/photo/1" },
+    { name: "ねこきのさん", src: "assets/gallery/fa-118-nekokino.jpg", twitter: "" },
   ];
   const WATERMARK_WHITE = "assets/images/watermark-no-repost-white.png";
   const WATERMARK_BLACK = "assets/images/watermark-no-repost-black.png";
